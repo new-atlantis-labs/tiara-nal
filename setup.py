e965e85a521bfa8ee1b91d6c971c14aeaa4e101b
Steve Sperandeo <steve.sperandeo@gmail.com>
2024-12-26 23:53:58 +0000

    Install works on newer python version.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.7,<3.10",
+    python_requires=">=3.7",
     keywords="machine-learning computational-biology",
     install_requires=requirements,
 )
